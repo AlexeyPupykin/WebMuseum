14c72465b3f75fb0ce207de520d007a847800fbf
mymailvrn98 <mymailvrn98@gmail.com>
2019-11-19 19:08:45 +0300

    сделал MVC "Создателя";

---
+++ Museum/Museum/Controllers/CreatorsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Museum.Models;
+
+namespace Museum.Controllers
+{
+    public class CreatorsController : Controller
+    {
+        private MuseumDBEntities db = new MuseumDBEntities();
+
+        List<Creator> creators = null;
+
+        public CreatorsController()
+        {
+            creators = db.Creators.ToList();
+        }
+
+        public ActionResult CreatorsView()
+        {
+            return View();
+        }
+
+        // GET: Creators
+        public ActionResult OutPutCreators()
+        {
+            return PartialView("_CreatorsViewList", creators);
+        }
+    }
+}
