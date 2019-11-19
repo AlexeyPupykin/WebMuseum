8888bc19585fd893d27e53e0ee2ed0dd68b20f22
mymailvrn98 <mymailvrn98@gmail.com>
2019-11-19 17:20:55 +0300

    создал проект Museum;

---
+++ Museum/Museum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Museum.Startup))]
+namespace Museum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
