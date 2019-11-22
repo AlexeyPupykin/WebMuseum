ad271c956d314ff2fa25ca90bb6e575c3535958f
mymailvrn98 <mymailvrn98@gmail.com>
2019-11-22 20:02:20 +0300

    сделал шаблон текущего зала;
    в нем отображаются все экспонаты, которые в этом зале находятся;

--- Museum/Museum/Controllers/RoomsController.cs
+++ Museum/Museum/Controllers/RoomsController.cs
@@ -18,9 +18,32 @@ namespace Museum.Controllers
             rooms = db.Rooms.ToList();
         }
 
+        public ActionResult CurrentRoomView(int Room, string Name)
+        {
+            ViewBag.Title = Name;
+
+            List<GetCurrentRoom_Result> view = GetCurrentRoomView(Room);
+
+            return View(view);
+        }
+
         public ActionResult RoomsView()
         {
             return View(rooms);
+        }
+
+        private List<GetCurrentRoom_Result> GetCurrentRoomView(int Room)
+        {
+            List<GetCurrentRoom_Result> resultset = null;
+
+            var prmIdRoom = new System.Data.SqlClient.SqlParameter("@p_Room", System.Data.SqlDbType.Int);
+            prmIdRoom.Value = Room;
+
+            var result = db.Database.SqlQuery<GetCurrentRoom_Result>("GetCurrentRoom @p_Room", prmIdRoom).ToList();
+
+            resultset = result;
+
+            return resultset;
         }
 
         // GET: Rooms
