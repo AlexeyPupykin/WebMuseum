4a85e0f80e59609eb350543380af34a0a5559cda
mymailvrn98 <mymailvrn98@gmail.com>
2019-12-13 02:59:01 +0300

    сделал функцию редактирования и удаления для создателей;

--- Museum/Museum/Controllers/CreatorsController.cs
+++ Museum/Museum/Controllers/CreatorsController.cs
@@ -42,6 +42,46 @@ namespace Museum.Controllers
             return View("CreatorsView");
         }
 
+        public ActionResult ChangeCreatorView(int id)
+        {
+            var c = creators.Find(x => x.IDCreator == id);
+
+            return View("ChangeCreatorView", c);
+        }
+
+        public ActionResult ChangeCreator(int id, string name, string country, string lifetime)
+        {
+            var prmid = new System.Data.SqlClient.SqlParameter("@IDCreator", System.Data.SqlDbType.Int);
+            prmid.Value = id;
+
+            var prmname = new System.Data.SqlClient.SqlParameter("@Name", System.Data.SqlDbType.NVarChar);
+            prmname.Value = name;
+
+            var prmcountry = new System.Data.SqlClient.SqlParameter("@Country", System.Data.SqlDbType.NVarChar);
+            prmcountry.Value = country;
+
+            var prmlifetime = new System.Data.SqlClient.SqlParameter("@Lifetime", System.Data.SqlDbType.NVarChar);
+            prmlifetime.Value = lifetime;
+
+            db.Database.ExecuteSqlCommand(
+                "UPDATE Creators SET " +
+                "Name = @Name, Counrty = @Country, Lifetime = @LIfetime " +
+                "WHERE IDCreator = @IDCreator"
+                , prmname, prmcountry, prmlifetime, prmid);
+
+            return View("CreatorsView");
+        }
+
+        public ActionResult DeleteCreator(int id)
+        {
+            var prmid = new System.Data.SqlClient.SqlParameter("@IDCreator", System.Data.SqlDbType.Int);
+            prmid.Value = id;
+
+            db.Database.ExecuteSqlCommand("DELETE FROM Creators WHERE IDCreator = @IDCreator", prmid);
+
+            return View("CreatorsView");
+        }
+
         // GET: Creators
         public ActionResult OutPutCreators()
         {
