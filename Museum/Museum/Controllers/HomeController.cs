eba8a56b117d3d09689ca705f49643cf151635de
mymailvrn98 <mymailvrn98@gmail.com>
2019-12-14 01:31:17 +0300

    выведены стили с информацией о них;
    доработаны мелочи;

--- Museum/Museum/Controllers/HomeController.cs
+++ Museum/Museum/Controllers/HomeController.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Museum.Models;
 
 namespace Museum.Controllers
 {
     public class HomeController : Controller
     {
+        private MuseumDBEntities db = new MuseumDBEntities();
+
+        List<Style> styles = null;
+        List<Models.Type> types = null;
+
+        public HomeController()
+        {
+            styles = db.Styles.ToList();
+            types = db.Types.ToList();
+        }
+
         public ActionResult Index()
         {
+            ViewBag.Styles = styles;
+            ViewBag.Types = types;
+
             return View();
         }
 
